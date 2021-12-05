1cac17137091f9d8f2db9f96c0d44742901dff48
evksin <73026357+evksin@users.noreply.github.com>
2021-12-05 22:28:44 +0300

    Add files changes

--- Beautifulsoup.py
+++ Beautifulsoup.py
@@ -45,3 +45,25 @@ print("")
 
 for item in lst:
     print(clean_item2(str(item)))
+    
+print("")
+
+url = "https://seaborn.pydata.org/examples/index.html"
+response = requests.get(url)
+
+soup = BeautifulSoup(response.content, 'html.parser')
+print(soup)
+
+lst = soup.find_all('p')
+
+for item in lst:
+    print(item)
+
+def clean_item(my_item):
+    position = my_item.find('</p>')
+    return my_item[3:position]
+    
+print("")    
+    
+for item in lst:
+    print(clean_item(str(item)))
